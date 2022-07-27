607243efba2500b6069db39792f35ec605ac653d
Spynder <Spynder@users.noreply.github.com>
2022-07-27 20:19:43 +0300

    Ranged spam model

--- creep.mjs
+++ creep.mjs
@@ -1,11 +1,17 @@
-import {getObjectsByPrototype, findClosestByPath, getRange} from 'game/utils';
-import {StructureSpawn, Creep, StructureContainer} from 'game/prototypes';
-import {RESOURCE_ENERGY, ERR_NOT_IN_RANGE} from 'game/constants';
+import {getObjectsByPrototype, findClosestByPath, findClosestByRange, getRange} from 'game/utils';
+import {StructureSpawn, Creep, StructureContainer, ConstructionSite} from 'game/prototypes';
+import {RESOURCE_ENERGY, ERR_NOT_IN_RANGE, HEAL} from 'game/constants';
 
 import {getSpawn, getEnemySpawn, getCreeps, getEnemyCreeps} from './functions.mjs';
 
 Creep.prototype.hauler = function() {
 	if(this.store.getUsedCapacity(RESOURCE_ENERGY) == this.store.getCapacity(RESOURCE_ENERGY)) {
+		if(getSpawn().store.getUsedCapacity(RESOURCE_ENERGY) == getSpawn().store.getCapacity(RESOURCE_ENERGY)) {
+			let cs = findClosestByRange(this, getObjectsByPrototype(ConstructionSite));
+			if(cs && this.build(cs)) {
+				this.moveTo(cs);
+			}
+		}
 		if(this.transfer(getSpawn(), RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 			this.moveTo(getSpawn());
 		}
@@ -17,32 +23,64 @@ Creep.prototype.hauler = function() {
 	}
 }
 Creep.prototype.attacker = function() {
-	let healer = getCreeps().find(creep => creep.role == "healer");
-	if(!healer) return;
+	//let healer = getCreeps().find(creep => creep.role == "healer");
 	let enemySpawn = getEnemySpawn();
+	let healer = findClosestByPath(this, getEnemyCreeps().filter(creep => getRange(this, creep) < 3 && creep.body.some(part => part == HEAL)));
 	let enemy = findClosestByPath(this, getEnemyCreeps().filter(creep => getRange(this, creep) < 5));
+	if(!this.healer && healer) {
+		this.healer = healer;
+	}
 	if(!this.lockedTarget && enemy) {
 		this.lockedTarget = enemy;
 	}
-	if(this.lockedTarget) {
-		this.attack(this.lockedTarget);
-		if(!this.lockedTarget.exists) {
-			this.lockedTarget = undefined; return;
+	//console.log(this.lockedTarget);
+	if(this.lockedTarget || this.healer) {
+		if(this.lockedTarget && !this.lockedTarget.exists) {
+			this.lockedTarget = undefined;
 		}
-		this.moveTo(this.lockedTarget);
+		if(this.healer && !this.healer.exists) {
+			this.healer = undefined;
+		}
+		if(!this.lockedTarget && !this.healer) return;
+
+		let target;
+		if((this.healer && getRange(this, this.healer) < 4) || !this.lockedTarget) {
+			target = this.healer;
+		} else {
+			target = this.lockedTarget;
+		}
+
+		if(this.rangedAttack(target)) {
+			this.heal(this);
+		}
+		
+		//if(!healer) return;
+		//if(getRange(this, healer) <= 1) {
+		if(getRange(this, target) > 3) {
+			this.moveTo(target);
+		}
+		else if(getRange(this, target) < 3) {
+			this.moveTo(getSpawn());
+		}
+		//}
 		return;
 	}
-	this.attack(enemySpawn);
-	if(getRange(this, healer) <= 1) {
-		this.moveTo(enemySpawn);
+	if(this.rangedAttack(enemySpawn)) {
+		this.heal(this);
 	}
+
+	//if(!healer) return;
+	//if(getRange(this, healer) <= 1) {
+		this.moveTo(enemySpawn);
+	//}
 }
 
 Creep.prototype.healer = function() {
 	let attacker = getCreeps().find(creep => creep.role == "attacker");
 	if(!attacker) return;
 	this.moveTo(attacker);
-	this.heal(attacker);
+	if(this.hits + 200 <= this.hitsMax) this.heal(this);
+	else this.heal(attacker);
 }
 
 Creep.prototype.run = function() {
--- main.mjs
+++ main.mjs
@@ -1,24 +1,68 @@
-import {getObjectsByPrototype, findClosestByPath, getRange} from 'game/utils';
-import {StructureSpawn, Creep, StructureContainer} from 'game/prototypes';
-import {MOVE, CARRY, ATTACK, HEAL, RESOURCE_ENERGY, ERR_NOT_IN_RANGE} from 'game/constants';
+import {getObjectsByPrototype, findClosestByPath, getRange, createConstructionSite} from 'game/utils';
+import {StructureSpawn, Creep, StructureContainer, StructureRampart, StructureWall, ConstructionSite} from 'game/prototypes';
+import {MOVE, CARRY, RANGED_ATTACK, HEAL, WORK, RESOURCE_ENERGY, ERR_NOT_IN_RANGE} from 'game/constants';
 import { } from '/arena';
 
 import './creep.mjs';
 import './spawn.mjs';
 import {getSpawn, getCreeps} from './functions.mjs';
 
+let initDone = false;
+
+function buildWalls() {
+	let spawn = getSpawn();
+	const structs = [
+		{x: -2, y: -2, type: StructureRampart},
+		{x: -1, y: -2, type: StructureWall},
+		{x: 0, y: -2, type: StructureWall},
+		{x: 1, y: -2, type: StructureWall},
+		{x: 2, y: -2, type: StructureRampart},
+
+		{x: -2, y: 2, type: StructureWall},
+		{x: -1, y: 2, type: StructureWall},
+		{x: 0, y: 2, type: StructureWall},
+		{x: 1, y: 2, type: StructureWall},
+		{x: 2, y: 2, type: StructureWall},
+
+		{x: -2, y: 1, type: StructureWall},
+		{x: -2, y: 0, type: StructureWall},
+		{x: -2, y: -1, type: StructureWall},
+
+		{x: 2, y: 1, type: StructureWall},
+		{x: 2, y: 0, type: StructureWall},
+		{x: 2, y: -1, type: StructureWall},
+		
+	]
+	structs.forEach(function(struct) {
+		let alreadyPlaced = getObjectsByPrototype(ConstructionSite).find(site => site.x == spawn.x + struct.x && site.y == spawn.y + struct.y);
+		if(!alreadyPlaced)
+			createConstructionSite(spawn.x + struct.x, spawn.y + struct.y, struct.type);
+	});
+}
+
+function init() {
+	if(initDone) return;
+
+	
+
+	initDone = true;
+}
+
 export function loop() {
-	let myCreeps = getCreeps();
-	let haulers = myCreeps.filter(creep => creep.role == "hauler");
-	let attacker = myCreeps.find(creep => creep.role == "attacker");
-	let healer = myCreeps.find(creep => creep.role == "healer");
+	init();
 	let spawn = getSpawn();
 
+	let myCreeps = getCreeps();
+	let haulers = myCreeps.filter(creep => creep.role == "hauler");
+	let attackers = myCreeps.filter(creep => creep.role == "attacker");
+	let healers = myCreeps.filter(creep => creep.role == "healer");
+
+	let leader;
 	myCreeps.forEach(creep => creep.run());
 
 	if(haulers.length < 2) {
 		console.log("Spawning hauler");
-		let hauler = spawn.spawnCreep([MOVE, CARRY]);
+		let hauler = spawn.spawnCreep([MOVE, CARRY, MOVE, CARRY, MOVE, CARRY]);
 		console.log(hauler);
 		if(hauler.error) return;
 		else {
@@ -27,20 +71,33 @@ export function loop() {
 		}
 	}
 
-	else if(!attacker) {
-		attacker = spawn.spawnCreep([MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, ATTACK, ATTACK]);
+	else if(true) {
+		let attacker = spawn.spawnCreep([MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK]);
 		if(attacker.error) return;
 		else {
+			if(!leader) {
+				leader = attacker.object;
+			}
 			attacker.object.role = "attacker";
 			return;
 		}
 	}
 
-	else if(!healer) {
-		healer = spawn.spawnCreep([MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, HEAL, HEAL]);
+	/*else if(helpers.length < 2) {
+		let helper = spawn.spawnCreep([MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK]);
+		if(helper.error) return;
+		else {
+			helper.object.role = "helper";
+			return;
+		}
+	}*/
+
+	else if(!healers.length) {
+		let healer = spawn.spawnCreep([MOVE, HEAL, HEAL, HEAL]);
 		if(healer.error) return;
 		else {
 			healer.object.role = "healer";
+			healer.object.leader = leader;
 			return;
 		}
 	}
